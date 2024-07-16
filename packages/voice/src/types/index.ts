c740c72b66c08c61849b0ca952d1a8e0fd0781d9
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-07-16 03:12:54 +0000

    @oncordjs/voice: Trying to fix something there

--- packages/voice/src/types/index.ts
+++ packages/voice/src/types/index.ts
@@ -1,6 +1,10 @@
+import { AudioResource, CreateAudioResourceOptions } from "@discordjs/voice";
 import { Guild, VoiceChannel } from "discord.js"
 
 export type VoiceOptions = {
     channel: VoiceChannel;
     guild: Guild;
+}
+export interface JoinVoiceOptions {
+    resources: CreateAudioResourceOptions<MediaMetadata>
 }
--- packages/voice/src/voice.ts
+++ packages/voice/src/voice.ts
@@ -1,24 +1,34 @@
-import { Client, Collection, Guild, VoiceChannel } from "discord.js";
-import { VoiceOptions } from "./types";
-import { AudioPlayer, AudioPlayerStatus, AudioResource, createAudioPlayer, createAudioResource, getVoiceConnection, joinVoiceChannel, NoSubscriberBehavior, StreamType, VoiceConnection } from '@discordjs/voice'
-import { Gateway } from '@oncordjs/client'
+import { Client, Collection, VoiceChannel } from "discord.js";
+import { JoinVoiceOptions, VoiceOptions } from "./types";
+import { AudioPlayer, AudioPlayerStatus, AudioResource, createAudioPlayer, createAudioResource, entersState, getVoiceConnection, joinVoiceChannel, NoSubscriberBehavior, StreamType, VoiceConnection, VoiceConnectionStatus } from '@discordjs/voice';
+import { Gateway } from '@oncordjs/client';
 import playdl from 'play-dl';
 import { Readable } from "stream";
 
 export interface Queue {
     songs: string[];
     connection: VoiceConnection | undefined;
+    player: AudioPlayer | undefined;
 }
+
 interface AudioPlayerMetadata {
     channel: VoiceChannel;
-    track: String;
+    track: string;
 }
+
 export class Voice {
     public connection: VoiceConnection | undefined;
     public volume: number = 100;
-    public player: AudioPlayer | undefined;
+    public player: AudioPlayer;
+
     public constructor() {
-        this.player?.on("stateChange", async (oldState, newState) => {
+        this.player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Pause,
+            },
+        });
+
+        this.player.on("stateChange", async (oldState, newState) => {
             if (
                 oldState.status !== AudioPlayerStatus.Idle &&
                 newState.status === AudioPlayerStatus.Idle
@@ -28,92 +38,139 @@ export class Voice {
                 ).metadata.channel;
                 const client = channel.client as Gateway;
                 const guildQueue = client?.queue.get(channel.guildId) as Queue;
-                if (!guildQueue.songs[0]) return;
+                if (guildQueue.songs.length === 0) return;
 
-                const stream = await playdl.stream(guildQueue?.songs[0], {
-                    quality: 2
-                })
+                const stream = await playdl.stream(guildQueue.songs[0], {
+                    quality: 2,
+                });
 
                 guildQueue.songs.shift();
-                await this.playUrl(stream.stream, channel);
+                await this.playUrl(stream.stream, channel, {
+                    resources: {
+                        inputType: stream.type,
+                    },
+                });
             }
-        })
+        });
     }
+
     public async connect(client: Client, options: VoiceOptions) {
-        this.connection = joinVoiceChannel({
+        const connection = joinVoiceChannel({
             channelId: options.channel.id,
             guildId: options.guild.id,
-            adapterCreator: options.guild.voiceAdapterCreator
-        })
-        return this.connection;
+            adapterCreator: options.guild.voiceAdapterCreator,
+        });
+
+        try {
+            await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
+            this.connection = connection;
+            return connection;
+        } catch (error) {
+            connection.destroy();
+            throw new Error('Failed to connect to the voice channel.');
+        }
     }
-    /**
-     * This thing is bugged for now, i'm trying to fix it.
-     */
-    private async play(song: string, channel: VoiceChannel) {
+
+    public async play(song: string, channel: VoiceChannel) {
         if (!song) return;
         const client = channel.client as Gateway;
         const queue = client.queue as Collection<string, Queue>;
         if (!queue) throw new Error("Can't find the Queue Collection.");
         const guildQueue = queue.get(channel.guildId);
         const stream = await playdl.stream(song, {
-            quality: 2
+            quality: 2,
         });
         if (!guildQueue) {
             if (!stream) throw new Error("Can't check song stream.");
 
+            await this.playUrl(stream.stream, channel, {
+                resources: {
+                    inputType: stream.type,
+                },
+            });
             const queueConstruct: Queue = {
                 songs: [],
-                connection: this.connection
-            }
+                connection: this.connection,
+                player: this.player,
+            };
             queueConstruct.songs.push(song);
 
-            await this.playUrl(stream.stream, channel);
             queue.set(channel.guildId, queueConstruct);
-            queueConstruct.songs.unshift(song)
-
         } else {
             guildQueue.songs.push(song);
-
         }
 
         return {
             queue,
             connection: this.connection,
-            player: this.player
-        }
+            player: this.player,
+        };
     }
-    public async playUrl(url: Readable | string, channel: VoiceChannel) {
-        this.connect(channel.client, {
-            channel: channel,
-            guild: channel.guild
-        });
 
-        this.player = createAudioPlayer({
-            behaviors: {
-                noSubscriber: NoSubscriberBehavior.Pause
-            }
-        });
-        const connection: VoiceConnection | undefined = getVoiceConnection(channel.guild.id);
-        if (!connection) return;
+    public async playUrl(url: Readable | string, channel: VoiceChannel, options?: JoinVoiceOptions) {
+        if (!this.connection) {
+            this.connection = await this.connect(channel.client, {
+                channel: channel,
+                guild: channel.guild,
+            });
+        }
+
+        if (!this.connection) return;
+
+        const connection = this.connection;
         const subscriber = connection.subscribe(this.player);
+
+        connection
+            .on(VoiceConnectionStatus.Disconnected, async () => {
+                try {
+                    await Promise.race([
+                        entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+                        entersState(connection, VoiceConnectionStatus.Connecting, 5_000),
+                    ]);
+                } catch (error) {
+                    connection.destroy();
+                }
+            })
+            .on(VoiceConnectionStatus.Destroyed, () => {
+                this.player?.stop();
+                subscriber?.unsubscribe();
+            });
+
         const resource = createAudioResource(url, {
             inlineVolume: true,
-            inputType: StreamType.Raw,
+            ...options?.resources,
             metadata: {
-                voiceChannel: channel
+                channel: channel,
             },
         });
+
+        const networkStateChangeListener = (
+            oldNetworkState: object,
+            newNetworkState: object,
+        ) => {
+            const newUdp = Reflect.get(newNetworkState, "udp");
+            clearInterval(newUdp?.keepAliveInterval);
+        };
+
+        connection.on("stateChange", (oldState, newState) => {
+            const oldNetworking = Reflect.get(oldState, "networking");
+            const newNetworking = Reflect.get(newState, "networking");
+
+            oldNetworking?.off("stateChange", networkStateChangeListener);
+            newNetworking?.on("stateChange", networkStateChangeListener);
+        });
+
         resource.volume?.setVolumeLogarithmic(this.volume / 100);
-        this.player.play(resource)
+        this.player.play(resource);
 
         return {
             connection,
             resource,
             subscriber,
-            player: this.player
-        }
+            player: this.player,
+        };
     }
+
     public setVolume(volume: number) {
         this.volume = volume;
         if (this.player?.state.status === AudioPlayerStatus.Playing) {
